619ee57d50a732510ea6e4790e907390a983d2d1
Martin Lastowski <martylastowski@stanfordalumni.org>
2026-05-11 16:37:08 -0700

    Complete unit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Ignore everything above this line
         System.out.println("Hello World!");
+        System.out.println("Goodbye!");
         // Ignore everything below this line
     }
 }
